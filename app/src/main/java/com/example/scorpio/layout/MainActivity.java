385b402f77c6f9e8a8ae2bf04ed2be629cb3a02c
Scorpio <Scorpio@MacBook-Pro.workgroup>
2016-04-11 19:09:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/scorpio/layout/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.scorpio.layout;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.unit_demo);
+    }
+}
